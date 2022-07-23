a4f93f0826ea91a3a3de1ee4657a913b37dca4bc
Sebastián Fuchilieri <sebastianm.fuchilieri@gmail.com>
2022-07-23 00:36:57 -0300

    cruzamiento y mutacion de individuos

--- Codigo/AgenteGenetico.py
+++ Codigo/AgenteGenetico.py
@@ -1,4 +1,4 @@
-from random import randint, triangular, uniform
+import random
 from Domain.objeto import Objeto
 import copy
 
@@ -17,16 +17,16 @@ class AgenteGenetico:
 
         while len(population) < 20:
 
-            rand_i = randint(0, (self.env.i / 2) - 1)
-            rand_j = randint(0, (self.env.j / 2) - 1)
-            rand_k = randint(0, (self.env.k / 2) - 1)
+            rand_i = random.randint(0, (self.env.i / 2) - 1)
+            rand_j = random.randint(0, (self.env.j / 2) - 1)
+            rand_k = random.randint(0, (self.env.k / 2) - 1)
             new_obj = Objeto(rand_i, rand_j, rand_k, self.obj.largo, self.obj.ancho, self.obj.alto)
 
             if self.env.validarPosicionVacia(new_obj):
                 if self.validarPosiciones():
                     population.append(new_obj)
         
-        return population
+        self.population = population
         
 
 
@@ -75,13 +75,48 @@ class AgenteGenetico:
     def calcularFitness(self):
         print("hola")
        
-    # Cruza 2 entornos y devuelve un entorno con "mejores resultados"
-    def cruzamiento(self):
-        print("hola")
+    # Cruza 2 objetos y devuelve ambos para despues quedarme con los mejores resultados
+    def cruzamiento(self, ind_1, ind_2):
+        posibilidades = ["x", "y", "z"]
+
+        new_ind_1 = Objeto(ind_1.x, ind_1.y, ind_1.z, self.obj.largo, self.obj.ancho, self.obj.alto)
+        new_ind_2 = Objeto(ind_2.x, ind_2.y, ind_2.z, self.obj.largo, self.obj.ancho, self.obj.alto)
+        for i in range(2):
+            value = random.choice(posibilidades)
+            posibilidades.remove(value)
+            print("vuelta: ", i )
+            print(value)
+            if value == "x":
+                holder = new_ind_1.x
+                new_ind_1.x = new_ind_2.x
+                new_ind_2.x = holder
+            elif value == "y":
+                holder = new_ind_1.y
+                new_ind_1.y = new_ind_2.y
+                new_ind_2.y = holder
+            elif value == "z":
+                holder = new_ind_1.z
+                new_ind_1.z = new_ind_2.z
+                new_ind_2.z = holder
+
+        return new_ind_1,new_ind_2
+
         
-    # Modifica algun parametro del entorno aleatoriamente
-    def mutacion(self):
-        print("hola")
+        
+    # Modifica algun parametro del objeto aleatoriamente
+    def mutacion(self, obj):
+        rnd = random.uniform(0, 1)
+        if rnd < 0.2:
+            posibilidades = ["x", "y", "z"]
+            value = random.choice(posibilidades)
+            if value == "x":
+                obj.x = random.randint(0, (self.env.i / 2) - 1)
+            elif value == "y":
+                obj.y = random.randint(0, (self.env.j / 2) - 1)
+            elif value == "z":
+                obj.z = random.randint(0, (self.env.k / 2) - 1)
+
+        return obj
         
         
     # Resuelve toda la logica del algoritmo genetico, aca deberia ir todo el codigo llamadp
